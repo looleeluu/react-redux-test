b1c32c276641e38747296c44cf281188e2b20f3e
looleeluu <ilya.sinegubkin@gmail.com>
2021-07-04 23:44:12 +0300

    change reducers

--- src/reducers/reducers.jsx
+++ src/reducers/reducers.jsx
@@ -1,31 +1,20 @@
 import _ from 'lodash';
 import { combineReducers } from 'redux';
 import { handleActions } from 'redux-actions';
+import { reducer as formReducer } from 'redux-form';
 import * as actions from '../actions/actions';
-
-// TEXT REDUCERS MOUNTING
-const textState = '';
-
-const addTaskTextHandler = () => '';
-
-const updateNewTaskTextHandler = (_state, { payload: { text } }) => text;
-
-const textHandlers = {
-  [actions.addTask]: addTaskTextHandler,
-  [actions.updateNewTaskText]: updateNewTaskTextHandler,
-};
-
-const textReducer = handleActions(textHandlers, textState);
 
 // TASKS REDUCERS MOUNTING
 const tasksState = {
   byId: {},
   allIds: [],
+  currentFilterName: 'all',
 };
 
 const addTaskHandler = (state, { payload: { task } }) => {
   const { byId, allIds } = state;
   return {
+    ...state,
     byId: { ...byId, [task.id]: task },
     allIds: [task.id, ...allIds],
   };
@@ -34,24 +23,32 @@ const addTaskHandler = (state, { payload: { task } }) => {
 const removeTaskHandler = (state, { payload: { id } }) => {
   const { byId, allIds } = state;
   return {
+    ...state,
     byId: _.omit(byId, id),
     allIds: _.without(allIds, id),
   };
 };
 
 const toggleTaskStateHandler = (state, { payload: { id } }) => {
-  const { byId, allIds } = state;
+  const { byId, allIds, currentFilterName } = state;
   const newTaskState = byId[id].state === 'active' ? 'finished' : 'active';
   return {
     byId: { ...byId, [id]: { ...byId[id], state: newTaskState } },
     allIds: [...allIds],
+    currentFilterName,
   };
 };
+
+const toggleTaskBoxFilter = (state, { payload: { filterName } }) => ({
+  ...state,
+  currentFilterName: filterName,
+});
 
 const tasksHandlers = {
   [actions.addTask]: addTaskHandler,
   [actions.removeTask]: removeTaskHandler,
   [actions.toggleTaskState]: toggleTaskStateHandler,
+  [actions.setTasksFilter]: toggleTaskBoxFilter,
 };
 
 const tasksReducer = handleActions(tasksHandlers, tasksState);
@@ -64,7 +61,9 @@ const addTaskUiStateHandler = (state, { payload: { task } }) => ({
   [task.id]: { theme: 'light' },
 });
 
-const inverseTaskThemeHandler = (state, { payload: { id } }) => {
+const removeTaskUiStateHandler = (state, { payload: { id } }) => _.omit(state, id);
+
+const toggleTaskThemeHandler = (state, { payload: { id } }) => {
   const newTheme = state[id].theme === 'light' ? 'primary' : 'light';
   return {
     ...state,
@@ -74,13 +73,14 @@ const inverseTaskThemeHandler = (state, { payload: { id } }) => {
 
 const tasksUiStateHandlers = {
   [actions.addTask]: addTaskUiStateHandler,
-  [actions.inverseTaskTheme]: inverseTaskThemeHandler,
+  [actions.removeTask]: removeTaskUiStateHandler,
+  [actions.toggleTaskTheme]: toggleTaskThemeHandler,
 };
 
 const tasksUiReducer = handleActions(tasksUiStateHandlers, tasksUiState);
 
 export default combineReducers({
-  text: textReducer,
   tasks: tasksReducer,
   tasksUiState: tasksUiReducer,
+  form: formReducer,
 });
