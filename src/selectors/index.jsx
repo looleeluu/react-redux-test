18cfa2c5564c9ef5564617394ca5f2d3f8aef9d2
looleeluu <ilya.sinegubkin@gmail.com>
2021-07-04 23:45:08 +0300

    add filterName selector

--- src/selectors/index.jsx
+++ src/selectors/index.jsx
@@ -2,11 +2,26 @@ import { createSelector } from 'reselect';
 
 const getTasks = (state) => state.tasks;
 
-// eslint-disable-next-line import/prefer-default-export
-export const tasksSelector = createSelector(
+const getCurrentFilterName = createSelector(
+  getTasks,
+  (tasks) => tasks.currentFilterName,
+);
+
+const tasksSelector = createSelector(
   getTasks,
   (tasks) => {
     const { byId, allIds } = tasks;
     return allIds.map((id) => byId[id]);
+  },
+);
+// eslint-disable-next-line import/prefer-default-export
+export const filteredTasksSelector = createSelector(
+  getCurrentFilterName,
+  tasksSelector,
+  (filterName, tasks) => {
+    if (filterName === 'all') {
+      return tasks;
+    }
+    return tasks.filter((t) => t.state === filterName);
   },
 );
